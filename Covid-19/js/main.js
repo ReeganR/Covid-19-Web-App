b5962af9be55452c9f58ebd371c95ba29fcf3b8f
ReeganR <53924039+ReeganR@users.noreply.github.com>
2020-10-14 20:54:26 +0200

    Add files via upload

---
+++ Covid-19/js/main.js
@@ -0,0 +1,46 @@
+const form = document.getElementById("myForm");
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const country = document.getElementById("country").value;
+    const url = "https://api.covid19api.com/total/dayone/country/"+country;
+    covidResult(url);
+    })
+
+    // async function covidResult(url){
+    //     let response = await fetch(url);
+    //     let data = await response.json()
+    //     console.log(data);
+    // }
+
+async function covidResult(url){
+    try{
+        const response = await fetch(url);
+        if(response.ok === false){
+            throw "Country not found";
+        }
+        const covidData = await response.json();
+        console.log(covidData);
+        const length = covidData.length;
+        const index = length - 1;
+
+        const confirmed = document.getElementById("confirmedCases");
+        const recovered = document.getElementById("recoveredCases");
+        const deaths = document.getElementById("deathCases");
+
+        confirmed.innerHTML = "";
+        recovered.innerHTML = ""; 
+        deaths.innerHTML = "";        
+
+        confirmed.append(covidData[index].Confirmed);
+        recovered.append(covidData[index].Recovered);
+        deaths.append(covidData[index].Deaths);
+
+    }
+    catch(err){
+        alert(err);
+    }
+}
+
+
